e077ab5f7785d7830a373c34071dcf67bd53f5e0
Luis de la Garza <delagarza@informatik.uni-tuebingen.de>
2014-03-25 15:13:59 +0100

    Added missing modules and packages

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from distutils.core import setup
 setup(
     name='CTDopts',
     version='0.3',
-    packages=[''],
+    packages=['CTDopts'],
     url='https://github.com/genericworkflownodes/CTDopts',
     license='',
     author='Andras Szolek',
     author_email='',
+    py_modules=['CTDopts/CTDopts'],
     description='A module for enabling tools with CTD reading/writing, argument parsing, validating and manipulating capabilities.'
 )
