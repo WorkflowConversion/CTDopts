28869bc18c671105f12b3cad1fa5f9eace02b572
Matthias Bernt <m.bernt@ufz.de>
2023-10-29 13:53:30 +0100

    black reformatted

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name='CTDopts',
-    version='1.5',
-    packages=['CTDopts'],
-    url='https://github.com/genericworkflownodes/CTDopts',
-    license='',
-    author='Andras Szolek',
-    author_email='',
-    py_modules=['CTDopts/CTDopts'],
-    description='A module for enabling tools with CTD reading/writing, argument parsing, validating and manipulating capabilities.'
+    name="CTDopts",
+    version="1.5",
+    packages=["CTDopts"],
+    url="https://github.com/genericworkflownodes/CTDopts",
+    license="",
+    author="Andras Szolek",
+    author_email="",
+    py_modules=["CTDopts/CTDopts"],
+    description="A module for enabling tools with CTD reading/writing, argument parsing, validating and manipulating capabilities.",
 )
